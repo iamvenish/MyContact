f9a0f2f9538306e952a7b569f7b9035ceef3cf6c
iamvenish <venish.pict21@sot.pdpu.ac.in>
2023-07-10 18:52:50 +0530

    MyContact

---
+++ vite-project/src/contact.jsx
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Venish",
+    imgURL: "src/IMG_20220114_180312_622.jpg",
+    phone: "+919825641854",
+    email: "Venishdpatel2602@gmail.com",
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
